ff67e56b31138de3b71c67195845857b60857216
Moon-YJ <tunjeong@gmail.com>
2023-11-20 10:59:18 +0900

    JSX 문법체계

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+	return (
+		<div className='App'>
+			<h1>Hello</h1>
+		</div>
+	);
+}
+
+export default App;
+
+/*
+	1. 컴포넌트 함수의 필수 조건
+		- 함수명을 대문자로 시작
+		- JSX를 리턴
+	
+	2. JSX
+		- 리액트에서 전용으로 쓰이는 DOM생성 문법
+		- 원래는 React.createElement()라는 메서드를 이용해서 복잡하게 구성해야하는 로직을 HTML태그를 따라한 친화적인 문법으로 편하게 DOM을 생성하기 위한 리액트만의 문법체계
+*/
