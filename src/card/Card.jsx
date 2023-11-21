1c0a92fb7141dff17e4d755f3cb94cc6bb0e78ee
Moon-YJ <tunjeong@gmail.com>
2023-11-21 12:18:21 +0900

    비구조화할당으로 props 전달받기

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,13 +1,13 @@
 import './Card.scss';
 
-export default function Card(props) {
+export default function Card({ colorName, index }) {
 	// 부모 컴포넌트에 전달되는 요소들을 props라는 객체로 묶여서 자식 컴포넌트의 파라미터 전달받음
-	console.log(props);
+	//console.log(props);
 	return (
 		<div className='Card '>
-			<div className='bgBox' style={{ backgroundColor: props.colorName }}></div>
+			<div className='bgBox' style={{ backgroundColor: colorName }}></div>
 			<div className='txtBox'>
-				{props.index + 1}: {props.colorName}
+				{index + 1}: {colorName}
 			</div>
 		</div>
 	);
