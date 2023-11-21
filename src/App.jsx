5540cbbb5b623c5db646b02a8ea462c806373381
Moon-YJ <tunjeong@gmail.com>
2023-11-21 12:14:14 +0900

    props를 이용해서 부모 컴포넌트에서 자식 컴포넌트로 데이터 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
+import Card from './card/Card';
+
 export default function App() {
-	const colors = ['orange', 'hotpink', 'aqua', 'green', 'purple', 'lightblue'];
+	const colors = ['orange', 'hotpink', 'aqua', 'green'];
 
 	return (
 		<>
 			<h1>Color Chart</h1>
-			<h2 style={{ color: 'blue' }}>Title2</h2>
-			<ul>
-				{colors.map((col, idx) => {
-					// return문에서 map을 통해 반복적으로 생성되는 가상돔요소에는 무조건 고유의 key값 줘야함
-					// react로 하여금 여러개의 반복요소중에서 수정되어야하는 요소를 효율적으로 비교탐색하기 위함
-					// 간편하게는 반복되는 순서값을 집어넣을수도 있긴하나 권장하진않으므로
-					// DB의 각 데이터에 내장되어 있는 Object_id를 활용하거나 실제 데이터와 순번을 결합해서 고유문자값 생성
-					return (
-						<li key={col + idx} style={{ color: col }}>
-							{col}
-						</li>
-					);
-				})}
-			</ul>
+			{colors.map((color, idx) => {
+				// 컴포넌트 호출시 특정값을 key={value} 형태로 컴포넌트내부에 전달 가능 (props)
+				return <Card key={color + idx} colorName={color} index={idx} />;
+			})}
 		</>
 	);
 }
