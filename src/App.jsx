2bb541174d53b14502deeaadf7d0947f31d69ba8
Moon-YJ <tunjeong@gmail.com>
2023-11-21 11:53:55 +0900

    map을 통한 동적 가상돔 반복 출력 및 key값 부여

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import Box from './box/Box';
+export default function App() {
+	const colors = ['orange', 'hotpink', 'aqua', 'green', 'purple', 'lightblue'];
 
-function App() {
-	return <Box />;
+	return (
+		<>
+			<h1>Color Chart</h1>
+			<h2 style={{ color: 'blue' }}>Title2</h2>
+			<ul>
+				{colors.map((col, idx) => {
+					// return문에서 map을 통해 반복적으로 생성되는 가상돔요소에는 무조건 고유의 key값 줘야함
+					// react로 하여금 여러개의 반복요소중에서 수정되어야하는 요소를 효율적으로 비교탐색하기 위함
+					// 간편하게는 반복되는 순서값을 집어넣을수도 있긴하나 권장하진않으므로
+					// DB의 각 데이터에 내장되어 있는 Object_id를 활용하거나 실제 데이터와 순번을 결합해서 고유문자값 생성
+					return (
+						<li key={col + idx} style={{ color: col }}>
+							{col}
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
 }
-
-export default App;
 
 /*
 	리액트에서의 re-render, re-paint
