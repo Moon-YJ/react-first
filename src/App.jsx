5bac59b8f0747d76adc05a5c8edeeb35f44ff1d1
Moon-YJ <tunjeong@gmail.com>
2023-11-22 13:15:19 +0900

    리액트에서 전위, 후위 증감 연산자 사용은 지양

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ export default function App() {
 
 	return (
 		<>
+			{/* 
+				- 후위연산자
+					: setIndex가 실행되는 순간에 0 (해당값의 초기값이 먼저 활용됨 = setIndex(0))
+				- 전위연산자
+					: setIndex가 실행되는 순간에 바로 초기값을 먼저 증가시킨뒤 호출되므로 setIndex(1)
+			*/}
 			<button onClick={() => setIndex(--Index)}>left</button>
 			<button onClick={() => setIndex(++Index)}>right</button>
 			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
