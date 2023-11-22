657867717d8acc96780959ca8ed6a14bcab741ea
Moon-YJ <tunjeong@gmail.com>
2023-11-22 13:10:49 +0900

    전위연산자, 후위연산자 및 state값 변경순서 고려

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
 import { useState } from 'react';
-
-/*
-	- State가 변경 및 반영되는 시점
-		1.이벤트 발생시 State변경함수로 변경될 때 해당 렌더링사이클에서 변경됨
-		2.변경된 State값이 실제 DOM에 반영되는 싸이클 그 다음번 렌더링 사이클 
-*/
+import './App.scss';
 
 export default function App() {
+	// state값 변경 순서에 따라 결과화면이 달라지는 경우
+	// 전위연산자, 후위연산자
 	console.log('render');
-	const [Txt, setTxt] = useState('old');
-
-	/*
-	- 위의 로직 실행순서
-		1. State값 초기화됨 (render1)
-		2. 버튼 클릭시 state값 변경은 되지만 해당 변경사항이 실제 반영되지 않음 (render1)
-		3. 리액트가 내부적으로 state값 변경을 인지해서 컴포넌트 재실행
-		4. 이전 렌더링사이클 변경된 state값이 실제 반영됨 (render2)
-	*/
+	let [Index, setIndex] = useState(0);
 
 	return (
 		<>
-			<h1>{Txt}</h1>
-			<button
-				onClick={() => {
-					setTxt('new');
-					console.log(Txt);
-				}}
-			>
-				글자변경
-			</button>
+			<button onClick={() => setIndex(--Index)}>left</button>
+			<button onClick={() => setIndex(++Index)}>right</button>
+			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
 		</>
 	);
 }
